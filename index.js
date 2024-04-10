d7d8a8e172b62e718ade7d8b03ac1ac3882455cf
Anton <41597155+antontidev@users.noreply.github.com>
2024-04-11 00:32:10 +0500

    Add mongodb connect

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
+const { MongoClient } = require('mongodb');
 
 // Токен вашего бота
-const token = process.env.BOT_TOKEN;
+const token = "7029767434:AAE8JBFLQaPdP4kerhSRuNbc8TdvhpqeWo0";
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'myProject';
 
 // Создание бота
 const bot = new TelegramBot(token, {polling: true});
+
+var db;
 
 // Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
@@ -30,3 +42,17 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, 'Пожалуйста, используйте кнопки для ответа на вопросы.');
   }
 });
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    db = client.db(dbName);
+  
+    return 'done.';
+  }
+  
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
